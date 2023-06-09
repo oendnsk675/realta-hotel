77c48eabacddd7c32a459b20cfd1054fcab1993e
osi <sayidinaahmadalqososyi@gmail.com>
2023-06-09 19:15:06 +0800

    add fitur upload multiple photo

--- app/admin/menu/photos/page.jsx
+++ app/admin/menu/photos/page.jsx
@@ -1,42 +1,108 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 
-const page = () => {
+const Page = () => {
+  const [photos, setPhotos] = useState([]);
+
+  const handlePhotoInputChange = (event, index) => {
+    const file = event.target.files[0];
+    const fileWithIndex = { ...file, index, isPrimary: false };
+    setPhotos((prevPhotos) => {
+      const newPhotos = [...prevPhotos];
+      newPhotos[index] = fileWithIndex;
+      return newPhotos;
+    });
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const dataURL = e.target.result;
+      setPhotos((prevPhotos) => {
+        const newPhotos = [...prevPhotos];
+        newPhotos[index] = { ...prevPhotos[index], dataURL };
+        return newPhotos;
+      });
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handlePrimaryPhotoChange = (event, index) => {
+    const isPrimary = event.target.checked;
+    setPhotos((prevPhotos) => {
+      const newPhotos = prevPhotos.map((photo, i) => {
+        if (i === index) {
+          return { ...photo, isPrimary };
+        } else if (isPrimary && photo.isPrimary) {
+          return { ...photo, isPrimary: false };
+        } else {
+          return photo;
+        }
+      });
+      return newPhotos;
+    });
+  };
+
+  const removePhoto = (index) => {
+    setPhotos((prevPhotos) => prevPhotos.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
       <h1 className="text-3xl mb-8 font-extrabold text-left">
-        <span className="blue_gradient">Edit Menu</span>
+        <span className="blue_gradient">Resto Menu Sop Buntut</span>
       </h1>
       <form className="flex flex-col gap-2">
-        <div className="flex items-center">
-          <label htmlFor="name" className="mr-3 w-24">
-            Resto Menu
-          </label>
-          <input
-            type="text"
-            className="rounded-md w-80 p-1 px-2 border"
-            placeholder="Type here"
-          />
-        </div>
-        <div className="flex items-center">
-          <label htmlFor="name" className="mr-3 w-24">
-            Price
-          </label>
-          <input
-            type="text"
-            className="rounded-md w-80 p-1 px-2 border"
-            placeholder="Type here"
-          />
-        </div>
-        <div className="flex items-center justify-start">
-          <label htmlFor="name" className="mr-3 w-24">
-            Available
-          </label>
-          <input
-            type="checkbox"
-            className="rounded-md p-1 px-2 border w-fit"
-            placeholder="Type here"
-          />
+        <div className="flex gap-3">
+          {[1, 2, 3, 4].map((index) => (
+            <div key={index} className="flex flex-col items-center">
+              <label
+                htmlFor={`photo-${index}`}
+                className="mr-3 h-48 w-48 rounded-md flex items-center justify-center p-1 px-2 border-2 box-content cursor-pointer"
+              >
+                {photos[index - 1] && (
+                  <img
+                    src={photos[index - 1].dataURL}
+                    alt={`Preview-${index}`}
+                    className="object-cover w-full h-full"
+                  />
+                )}
+                {!photos[index - 1] && `Photo ${index}`}
+              </label>
+              <input
+                type="file"
+                className="hidden"
+                id={`photo-${index}`}
+                placeholder="Type here"
+                onChange={(event) => handlePhotoInputChange(event, index - 1)}
+              />
+              {photos[index - 1] && (
+                <div className="flex gap-2 flex-col mt-3 items-center">
+                  <div className="flex gap-2 items-center">
+                    <label htmlFor={`primary-${index}`}>
+                      Set as Primary Photo
+                    </label>
+                    <input
+                      type="checkbox"
+                      id={`primary-${index}`}
+                      checked={photos[index - 1].isPrimary}
+                      onChange={(event) =>
+                        handlePrimaryPhotoChange(event, index - 1)
+                      }
+                    />
+                  </div>
+                  {!photos[index - 1].isPrimary && (
+                    <button
+                      className="bg-red-500 text-white px-2 py-1 rounded-md"
+                      onClick={() => removePhoto(index - 1)}
+                    >
+                      Remove
+                    </button>
+                  )}
+                </div>
+              )}
+            </div>
+          ))}
         </div>
 
         <div className="flex gap-2 mt-5">
@@ -58,4 +124,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
